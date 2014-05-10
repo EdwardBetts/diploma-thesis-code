62438a495ac17cf12dcf287bb564109701863837
mokapharr <tobiasjammer@googlemail.com>
2014-05-10 16:36:29 +0200

    linepars added

--- scripts/pde_pscut.py
+++ scripts/pde_pscut.py
@@ -6,10 +6,11 @@ from spectools import get_guess, get_cutoff, get_nmean_errors
 from pdetools import calc_pde_with_errors, correct_wavelength
 
 
-def extract_pde(dkcts, int_start, print_pe=False, linepars=None):
+def extract_pde(dkcts, int_start, print_pe=False, linepars=None,
+                qe_loc='/home/jammer/diplom/calib/pmt2c.dat'):
     filelist = [f for f in glob('*_*') if not '.' in f]
     wavelengths = unique(i[:-2] for i in filelist)
-    QE_file = '/home/jammer/diplom/calib/pmt2c.dat'
+    QE_file = qe_loc
     f = loadtxt(QE_file, unpack=True)
 
     pde_array = []
@@ -29,7 +30,7 @@ def extract_pde(dkcts, int_start, print_pe=False, linepars=None):
                 a, b = get_line(intd, mind)
             else:
                 a, b = linepars
-            sipm_hist = get_hist(intd, mind, b)[0]
+            sipm_hist = get_hist(intd, mind, (a, b))[0]
 
             with open(fname, 'r') as fl:
                 my_dtype = return_dtype(2)
