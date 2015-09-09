0fd15220d10d3eb45dab4ca9348cea89140c6418
mokapharr <tobiasjammer@googlemail.com>
2015-09-09 10:24:02 +0200

    fixed pep8 errors

--- scripts/pdetools.py
+++ scripts/pdetools.py
@@ -8,7 +8,7 @@ def calculate_pde(A_1, A_2, B_1, B_2, pde_pmt, dkcts):
 
 def correct_wavelength(wavelength):
     angle = arctan(.75 / 3)
-    #given by the manufacturor of the ir-filters
+    # given by the manufacturor of the ir-filters
     k = 0.11
     factor = 1 - k * sin(angle) * sin(angle)
 
@@ -18,22 +18,23 @@ def correct_wavelength(wavelength):
 def calc_pde_with_errors(A1, A2, B1, B2, qe_pmt, dkcts):
     a = 0.006
     try:
-        factor = (A1[0]-dkcts[0]) * (A2[0]-dkcts[0]) / (B1[0]-a) / (B2[0]-a)
+        factor = (A1[0] - dkcts[0]) * (A2[0] - dkcts[0]) / \
+            (B1[0] - a) / (B2[0] - a)
     except TypeError:
         factor = (A1[0] - dkcts) * (A2[0] - dkcts) / B1[0] / B2[0]
         dkcts = (dkcts, dkcts / 2)
 
     pde = sqrt(factor) * qe_pmt
-    #statistical error:
+    # statistical error:
     sterr = sqrt((0.5 * factor**(0.5) / A1[0] * A1[1])**2 +
                  (0.5 * factor**(0.5) / A2[0] * A2[1])**2 +
                  (- 0.5 * factor**(0.5) / B1[0] * B1[1])**2 +
                  (- 0.5 * factor**(0.5) / B2[0] * B2[1])**2 +
                  (0.5 * factor / B1[0] / B2[0] *
                   (2 * dkcts[0] - A1[0] - A2[0]) * dkcts[1])**2) * qe_pmt
-    #systematic error:
+    # systematic error:
     syserr = sqrt((0.5 * factor**(0.5) / A1[0] * A1[2])**2 +
-                 (0.5 * factor**(0.5) / A2[0] * A2[2])**2 +
-                 (- 0.5 * factor**(0.5) / B1[0] * B1[2])**2 +
-                 (- 0.5 * factor**(0.5) / B2[0] * B2[2])**2) * qe_pmt
+                  (0.5 * factor**(0.5) / A2[0] * A2[2])**2 +
+                  (- 0.5 * factor**(0.5) / B1[0] * B1[2])**2 +
+                  (- 0.5 * factor**(0.5) / B2[0] * B2[2])**2) * qe_pmt
     return pde, sterr, syserr
